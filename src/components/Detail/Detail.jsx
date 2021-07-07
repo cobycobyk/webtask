a1e9f12e64c64f293636e5cc381a93462053e885
cobycobyk <ckabili8@gmail.com>
2021-07-06 18:23:05 -0600

    style detail component

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -4,29 +4,31 @@ import MailIcon from '@material-ui/icons/Mail';
 export default function Detail({car}) {
   return (
     <div className="detail">
-      <h1>{car.name}</h1>
-      <h7>Year</h7>
-      <h4>{car.year}</h4>
-      <h7>Price Range</h7>
-      <h4>{car.priceLow} - {car.priceHigh}</h4>
-      <h7>Item Number: #{car._id?.slice(11,16)}</h7>
-      <h7>VIN: {car.vin}</h7>
+      <h4 className="detail__hero">{car.name}</h4>
+      <h4 className="detail__option">Year</h4>
+      <h4 className="detail__insert">{car.year}</h4>
+      <h4 className="detail__option">Price Range</h4>
+      <h4 className="detail__insert">${car.priceLow} - ${car.priceHigh}</h4>
+      <h4 className="detail__option">Mileage</h4>
+      <h4 className="detail__insert">{car.mileage} miles</h4>
+      <h4 className="detail__option">Item Number: #{car._id?.slice(11,16)}</h4>
+      <h4 className="detail__option">VIN: {car.vin}</h4>
       <div className="detail__share">
-        <h8>Share this car</h8>
+        <h4>Share this car</h4>
         <MailIcon className="detail__icon"/>
       </div>
       <div className="detail__views">
         <div className="detail__viewsoption">
-          <h7>Views</h7>
-          <h4>37</h4>
+          <h4 className="detail__option">Views</h4>
+          <h4 className="detail__count" style={{color: "var(--green)"}}>37</h4>
         </div>
         <div className="detail__viewsoption">
-          <h7>Saves</h7>
-          <h4>20</h4>
+          <h4 className="detail__option">Saves</h4>
+          <h4 className="detail__count" style={{color: "var(--green)"}}>20</h4>
         </div>
         <div className="detail__viewsoption">
-          <h7>Shares</h7>
-          <h4>15</h4>
+          <h4 className="detail__option">Shares</h4>
+          <h4 className="detail__count" style={{color: "var(--green)"}}>15</h4>
         </div>
       </div>
     </div>
--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -4,6 +4,7 @@ export default function Image({image, setImage}) {
   return (
     <div className="image">
       <img onClick={() => setImage(image)} src={image} alt="" />
+      &nbsp;
     </div>
   )
 }
